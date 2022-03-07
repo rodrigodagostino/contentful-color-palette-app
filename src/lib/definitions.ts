47d85450d9e9aa03a2c9a56705e55b47f91a6da3
Rodrigo D’Agostino <rodrigo.dagostino@gmail.com>
2022-03-06 22:04:07 -0300

    chore: Add definitions

---
+++ src/lib/definitions.ts
@@ -0,0 +1,164 @@
+import { ColorTokenType } from './types'
+
+export const COLOR_TOKENS: ColorTokenType = {
+  slate: {
+    label: 'Slate',
+    hex: '0d1d32',
+    rgba: {
+      r: '13',
+      g: '29',
+      b: '50',
+      a: '1',
+    },
+  },
+  blue: {
+    label: 'Blue',
+    hex: '2356f6',
+    rgba: {
+      r: '35',
+      g: '86',
+      b: '246',
+      a: '1',
+    },
+  },
+  aqua: {
+    label: 'Aqua',
+    hex: '6dc4ec',
+    rgba: {
+      r: '109',
+      g: '196',
+      b: '236',
+      a: '1',
+    },
+  },
+  ice: {
+    label: 'Ice',
+    hex: 'caf4f7',
+    rgba: {
+      r: '202',
+      g: '244',
+      b: '247',
+      a: '1',
+    },
+  },
+  sky: {
+    label: 'Sky',
+    hex: 'cadffa',
+    rgba: {
+      r: '202',
+      g: '223',
+      b: '250',
+      a: '1',
+    },
+  },
+  skyLight: {
+    label: 'Sky light',
+    hex: 'e4eefa',
+    rgba: {
+      r: '228',
+      g: '238',
+      b: '250',
+      a: '1',
+    },
+  },
+  stone: {
+    label: 'Stone',
+    hex: 'f1f1f1',
+    rgba: {
+      r: '202',
+      g: '223',
+      b: '250',
+      a: '1',
+    },
+  },
+  gray: {
+    label: 'Gray',
+    hex: 'e6e6e6',
+    rgba: {
+      r: '230',
+      g: '230',
+      b: '230',
+      a: '1',
+    },
+  },
+  tangerine: {
+    label: 'Tangerine',
+    hex: 'fca04d',
+    rgba: {
+      r: '252',
+      g: '160',
+      b: '77',
+      a: '1',
+    },
+  },
+  tangerineLight: {
+    label: 'Tangerine light',
+    hex: 'fcc692',
+    rgba: {
+      r: '252',
+      g: '198',
+      b: '146',
+      a: '1',
+    },
+  },
+  coral: {
+    label: 'Coral',
+    hex: 'fc6563',
+    rgba: {
+      r: '252',
+      g: '101',
+      b: '99',
+      a: '1',
+    },
+  },
+  coralLight: {
+    label: 'Coral light',
+    hex: 'ffa19f',
+    rgba: {
+      r: '255',
+      g: '161',
+      b: '159',
+      a: '1',
+    },
+  },
+  violet: {
+    label: 'Violet',
+    hex: '5068bf',
+    rgba: {
+      r: '80',
+      g: '104',
+      b: '191',
+      a: '1',
+    },
+  },
+  violetLight: {
+    label: 'Violet light',
+    hex: '96a4d8',
+    rgba: {
+      r: '150',
+      g: '164',
+      b: '216',
+      a: '1',
+    },
+  },
+  white: {
+    label: 'White',
+    hex: 'fff',
+    rgba: {
+      r: '255',
+      g: '255',
+      b: '255',
+      a: '1',
+    },
+  },
+  transparent: {
+    label: 'Transparent',
+    hex: null,
+    rgba: {
+      r: '255',
+      g: '255',
+      b: '255',
+      a: '0',
+    },
+  },
+}
